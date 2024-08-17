b3e06d19bb4256aca71489315b64fe5d73186eb9
みのるん <74597894+minorun365@users.noreply.github.com>
2024-08-17 18:50:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ if "chain" not in st.session_state:
     # チャット用LLMを定義
     chat = ChatBedrock(
         model_id="anthropic.claude-3-haiku-20240307-v1:0",
-        model_kwargs={"max_tokens": 4000},
-        streaming=True,
+        model_kwargs={"max_tokens": 4000}, streaming=True,
     )
 
     # チェーンを定義
